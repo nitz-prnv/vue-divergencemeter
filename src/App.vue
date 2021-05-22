eb796c6179bf271f06e14f53e6676ed77da27de1
nitz-prnv <nitprnv@gmail.com>
2021-05-22 15:18:07 +0530

    added json

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,33 @@
       href="https://fonts.googleapis.com/css2?family=Nixie+One&display=swap"
       rel="stylesheet"
     />
-    
-    <div @click="SetDivergence" id="display" v-if="divergence">
-      <div v-for="value in divergence" v-bind:key="value" id="number">
+
+    <div @click="SetDivergence" id="display" >
+      <div v-for="value in divergence.value" v-bind:key="value[2]" id="number">
         <div>{{ value }}</div>
       </div>
+      <div></div>
     </div>
-    <!-- <img src="https://fanart.tv/api/download.php?type=download&image=40282&section=1" alt=""> -->
-    <UnderConstruction/>
+    <div id="name">{{ divergence.name }}</div>
   </div>
 </template>
 
 <script>
-import UnderConstruction from './components/UnderConstruction'
+// import UnderConstruction from './components/UnderConstruction'
+import values from "./values.json";
 export default {
   name: "App",
 
-  components: {UnderConstruction},
-
   data() {
     return {
-      divergence: '1.048596',
+      divergence: values[0],
+      values: values,
     };
   },
   methods: {
+   
     SetDivergence() {
-      let StrDiv = "0.328403";
-      this.divergence = StrDiv.split("");
+      this.divergence= values[Math.floor(Math.random() * values.length)];
     },
   },
 };
@@ -45,16 +45,22 @@ export default {
   /* align-self: center; */
   color: rgb(253, 178, 124);
   background-color: rgb(27, 23, 23);
-  text-shadow: 1px 1px 2px rgb(235, 90, 53), 0 0 20px rgb(255, 60, 0),0 0 8px rgb(139, 9, 0);
+  text-shadow: 1px 1px 2px rgb(235, 90, 53), 0 0 20px rgb(255, 60, 0),
+    0 0 8px rgb(139, 9, 0);
   text-align: center;
   font-size: 9vw;
   position: relative;
   width: auto;
   border: 2vw double rgb(196, 193, 188);
   border-radius: 3vw 3vw 0px 0px;
-  border-bottom:1vw double rgb(196, 193, 188) ;
-  border-top:1vw solid rgb(196, 193, 188) ;
+  border-bottom: 1vw double rgb(196, 193, 188);
+  border-top: 1vw solid rgb(196, 193, 188);
   font-family: "Nixie One", cursive;
+}
+#name{
+  text-align: center;
+  color: aliceblue;
+  font-size: 3em;
 }
 #number {
   display: flex;
@@ -63,22 +69,26 @@ export default {
   justify-content: center;
   padding: 1%;
   margin-top: 1%;
-  margin-left: 1% ;
-  margin-right: 1% ;
-  width:7%;
+  margin-left: 1%;
+  margin-right: 1%;
+  width: 7%;
   border: 2px solid wheat;
-  background-color: rgba(56, 26, 26, 0.575);
+  background-color: rgba(87, 35, 35, 0.575);
   border-radius: 2vw 2vw 0px 0px;
-  
 }
-#app{
+#app {
   padding: 1%;
   display: flex;
   flex-direction: column;
-  
 }
-html{
+html {
   background-color: black;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
-
 </style>
